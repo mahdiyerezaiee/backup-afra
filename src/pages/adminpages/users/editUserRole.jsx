4d114d0ce76843626c161d6c8a5f64098c7ca5e3
mahmood <mahmoodhormati@gmail.com>
2022-12-13 16:38:31 +0330

    change userrole and usergroup

--- src/pages/adminpages/users/editUserRole.jsx
+++ src/pages/adminpages/users/editUserRole.jsx
@@ -1,16 +1,32 @@
-import React, {useCallback, useEffect, useState} from "react";
-import {GetUserInfo, GetUsersRoles, SetUserRole} from "../../../services/userService";
-import {useNavigate, useParams} from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { GetUserInfo, GetUsersRoles, SetUserRole } from "../../../services/userService";
+import { useNavigate, useParams } from "react-router-dom";
 import Select from "react-select";
-import {NavLink} from "react-router-dom";
-import {toast} from "react-toastify";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
 import { optionsRole } from "../../../Enums/RolesEnums";
-import {addUserInfo} from "../../../actions/user";
+import { addUserInfo } from "../../../actions/user";
+import Modal from 'react-modal';
 
 
+const customStyles = {
+    content: {
 
-const EditUserRole = () => {
-    const params=useParams()
+        inset: '50% auto auto 50%',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        borderRadius: '30px',
+        border: '2px ridge black'
+    }
+
+}
+
+
+const EditUserRole = ({id,closeModal,modalIsOpen}) => {
     const [editRoles, setEditRoles] = useState([])
     const [roles, setRoles] = useState([]);
     const navigate = useNavigate()
@@ -18,7 +34,7 @@ const EditUserRole = () => {
 
     const getrole = async () => {
         try {
-            const {data, status} = await GetUsersRoles(Number(params.id))
+            const { data, status } = await GetUsersRoles(Number(id))
             setRoles(data.result.userRoleIds)
 
         } catch (error) {
@@ -28,116 +44,145 @@ const EditUserRole = () => {
     }
 
     useEffect(() => {
-
-            getrole();
-        }
+if(id>0){
+        getrole();
+}
+    }
         , [])
 
     const UserRoles = () => {
         if (!roles[0]) {
-    return (<span>نامشخص </span>)
-}
-
-
-if (roles[0] === 1) {
-    return (<span>ثبت نام شده </span>)
-}
-if (roles[0] === 2) {
-    return (<span> مشتری</span>)
-}
-if (roles[0] === 3) {
-    return (<span> کارمند</span>)
-}
-if (roles[0] === 4) {
-    return (<span> کارشناس پشتیبانی</span>)
-}if (roles[0] === 5) {
-    return (<span>ادمین مالی</span>)
-}
-if (roles[0] === 6) {
-    return (<span> ادمین انبار</span>)
-}
-if (roles[0] === 7) {
-    return (<span> ادمین</span>)
-}
-if (roles[0] === 8) {
-    return (<span> سوپر ادمین</span>)
-}
-}
-const userRoles = {
-    userRoleIds: [
-        editRoles
-    ],
-    userId: params.id
-}
-    const setRole =async () => {
-
-      const {data, status} = await SetUserRole(userRoles)
-           try {
-        
-        if (status === 200) {
-
-            toast.success("اطلاعات با موفقیت ثبت شد", {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: true,
-                progress: undefined
-            });
-
-            navigate('/userlist')
-
+            return (<span>نامشخص </span>)
         }
-            
-           } catch (error) {
+
+
+        if (roles[0] === 1) {
+            return (<span>ثبت نام شده </span>)
+        }
+        if (roles[0] === 2) {
+            return (<span> مشتری</span>)
+        }
+        if (roles[0] === 3) {
+            return (<span> کارمند</span>)
+        }
+        if (roles[0] === 4) {
+            return (<span> کارشناس پشتیبانی</span>)
+        } if (roles[0] === 5) {
+            return (<span>ادمین مالی</span>)
+        }
+        if (roles[0] === 6) {
+            return (<span> ادمین انبار</span>)
+        }
+        if (roles[0] === 7) {
+            return (<span> ادمین</span>)
+        }
+        if (roles[0] === 8) {
+            return (<span> سوپر ادمین</span>)
+        }
+    }
+    const userRoles = {
+        userRoleIds: [
+            editRoles
+        ],
+        userId: id
+    }
+    const setRole = async () => {
+
+        const { data, status } = await SetUserRole(userRoles)
+        try {
+
+            closeModal()
+
+            if (status === 200) {
+
+                toast.success("اطلاعات با موفقیت ثبت شد", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: true,
+                    progress: undefined
+                });
+
+
+            }
+
+        } catch (error) {
             console.log(error);
-           }
+        }
 
     }
 
 
-const onchangRole = (e) => {
+    const onchangRole = (e) => {
         setEditRoles(e.value)
     }
 
-  return(
-      <div className='user-progress' >
-          <div className='row'>
-              <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12 p-3 m-2'>
-                  <h5 >تعیین نقش کاربر</h5>
-            <p>در این بخش می توانید نقش کاربر را ویرایش کنید</p>
+    return (
+        <Modal
 
-              </div>
-          </div>
-          <div className='row d-flex justify-content-center '>
-              <div className='widget box shadow col-md-4 col-xs-12'>
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Selected Option"
+            ariaHideApp={false}
 
-              <div className="form-group textOnInput  align-content-between">
+        >
 
-              <div className="form-group mb-4 textOnInput">
-                  <label>تعیین نقش</label>
-
-              <Select
-                  className="opacityForInput"
-      options={optionsRole}
-      placeholder={UserRoles()}
-      onChange={onchangRole}
-  />
-              </div>
-      <div className='row '>
-          <div className='col-6 '>
-              <button onClick={setRole} className="btn btn-success float-right" >تایید</button>
-          </div>
-          <div className='col-6 '>
-              <NavLink to="/userlist" className="btn btn-danger float-left">بازگشت</NavLink>
-          </div>
-      </div>
-      </div>
-      </div>
+            <div className="d-block clearfix mb-2" onClick={closeModal}><svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24"
+                viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-x close"
+                data-dismiss="alert"><line x1="18" y1="6"
+                    x2="6"
+                    y2="18"></line><line
+                        x1="6" y1="6" x2="18" y2="18"></line></svg></div>
+            <div>
+                <div className="card-body p-0" style={{ height: '14rem', width: '20rem' }}>
 
 
-  </div>
-  </div>)
+                    <div className="text-center mb-5">
+                        <h5 className="text-center">نوع کاربر</h5>
+                    </div>
+                    <div className="form-row mt-4">
+                        <div className="  form-group col-md-12 col-xs-12 textOnInput  selectIndex">
+
+
+                            <form>
+                                <div className='form-group'>
+
+                                    <div className="form-group mb-3">
+
+                                        <Select
+                                            className="opacityForInput"
+                                            options={optionsRole}
+                                            placeholder={UserRoles()}
+                                            onChange={onchangRole}
+                                            maxMenuHeight='100px'
+                                        />
+                                    </div>
+
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                    <div className='text-center mt-2'>
+
+                        <div className='col-12 '>
+                            <button className="btn btn-success  "
+                                onClick={setRole}>تایید
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </Modal>)
 }
 export default EditUserRole
