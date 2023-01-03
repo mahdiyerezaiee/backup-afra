5282985af15659b6714da5d94ffaa331536e61a4
mahdiye <103319316+mahdiyerezaiee@users.noreply.github.com>
2023-01-03 08:39:25 +0330

    bug/delete animation components

--- src/pages/adminpages/dashboard/dashbordAdmin.js
+++ src/pages/adminpages/dashboard/dashbordAdmin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ChartMain from "./chartMain";
 import News from '../../news/news';
 import SalesBoardForAdmin from './../../../components/common/salesBoardForAdmin';
@@ -7,15 +7,43 @@ import OverDuePaymentsByAttachments from "../../../components/common/OverDuePaym
 import ScheduleJob from "../report/ScheduleJob";
 
 const DashbordAdmin = () => {
+    useEffect(()=>{
+        function reveal() {
+            var reveals = document.querySelectorAll(".reveal");
+
+            for (var i = 0; i < reveals.length; i++) {
+                var windowHeight = window.innerHeight;
+                var elementTop = reveals[i].getBoundingClientRect().top;
+                var elementVisible = 150;
+
+                if (elementTop < windowHeight - elementVisible) {
+                    reveals[i].classList.add("active");
+                }
+                    // else {
+                //     reveals[i].classList.remove("active");
+                // }
+            }
+        }
+
+        document.getElementsByClassName("main-Layout")[0].addEventListener("scroll", reveal);
+    },[window])
   return(
       <div >
+          <div className="">
           <OverDuePaymentsByAttachments/>
-          <ReportBlock/>
+      </div>
+          <div className=" ">
+              <ReportBlock/>
+          </div>
+          <div className="reveal fade-right">
+              <ChartMain />
+          </div>
+          <div className="reveal fade-bottom">
+              <SalesBoardForAdmin/>
+          </div>
 
-          <ChartMain />
 
-          <SalesBoardForAdmin/>
-<div className="row mt-3">
+<div className="row mt-3 reveal fade-left">
     <div className="col-lg-6 col-sm-12"> <News/></div>
     <div className="col-lg-6 col-sm-12"><ScheduleJob/></div>
 </div>
