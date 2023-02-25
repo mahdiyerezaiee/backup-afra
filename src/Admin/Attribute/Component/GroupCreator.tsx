45018d11b106ecdb60ef6284c48186ea9e99c3e0
mahdiye <103319316+mahdiyerezaiee@users.noreply.github.com>
2023-02-25 16:31:03 +0330

    Feat/change-reportPayment-and-Service

--- src/Admin/Attribute/Component/GroupCreator.tsx
+++ src/Admin/Attribute/Component/GroupCreator.tsx
@@ -1,49 +1,11 @@
 import React,{useState} from 'react'
-import { EntityTypes } from '../../../Common/Enums/EntityTypesEnums';
 import  Select  from 'react-select';
 import { NavLink } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { SetGroup } from '../../../services/GroupService';
+import { entityTypes, submitGroupCreator } from '../service/GroupCreatorService';
 
 const GroupCreator:React.FC = () => {
-    const [name, setName] = useState('');
-    const [entityTypeId, setEntityTypeId] = useState(0);
-    const entityTypes = () => {
-        return (EntityTypes.map(data => ({ label: data.name, value: data.id })));
-    }
-
-    const body={
-        group:{
-            id:0,
-            entityTypeId,
-            name
-        }
-    }
-    const submit = async (event:any) => {
-        event.preventDefault();
-        try {
-
-            const { data, status } = await SetGroup(body);
-            if (status === 200) {
-                toast.success('گروه جدید ایجاد شد',
-                    {
-                        position: "top-right",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: false,
-                        draggable: true,
-                        progress: undefined
-                    })
-            }
-
-        } catch (error) {
-           
-        }
-
-
-    }
-  return (
+  
+    return (
     <div className='user-progress' >
             <div className='row'>
                 <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12 p-3 m-2'>
@@ -53,23 +15,23 @@ const GroupCreator:React.FC = () => {
             </div>
             <div className='row d-flex justify-content-center '>
                 <div className=' col-md-8 col-lg-8 col-sm-11 m-2'>
-                    <form className='form row '>
+                    <form className='form row ' onSubmit={submitGroupCreator}>
                         <div className=" col-md-6 col-sm-12 form-group mb-4 textOnInput  ">
 
                             <label >نام گروه</label>
-                            <input type="text" className="form-control opacityForInput" value={name} onChange={(e:any) => setName(e.target.value)} />
+                            <input type="text" name="name" className="form-control opacityForInput"  />
 
                         </div>
                         <div className=" col-md-6 col-sm-12 form-group mb-4 textOnInput  ">
 
 
-                            <Select className='form-input opacityForInput' placeholder='انتخاب موجودیت'
+                            <Select name='entityTypeId' className='form-input opacityForInput' placeholder='انتخاب موجودیت'
                                 options={entityTypes()}
-                                onChange={(e:any) => setEntityTypeId(e.value)} />
+                                 />
                         </div>
                    
                         <div className='col-6  '>
-                                <button type="submit" className="btn btn-success  float-right" onClick={submit} >تایید</button>
+                                <button type="submit" className="btn btn-success  float-right" >تایید</button>
                                 </div>
 
                                 <div className='col-6  '>
