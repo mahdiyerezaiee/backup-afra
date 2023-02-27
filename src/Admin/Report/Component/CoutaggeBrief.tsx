7a040288432f579cdec39da9bd1b314b891be72e
mahdiye <103319316+mahdiyerezaiee@users.noreply.github.com>
2023-02-27 13:47:02 +0330

    coutaggeBrife change

--- src/Admin/Report/Component/CoutaggeBrief.tsx
+++ src/Admin/Report/Component/CoutaggeBrief.tsx
@@ -21,16 +21,23 @@ const CoutaggeBrief: React.FC = () => {
     }
   };
   useEffect(() => {
-    GetCoutaggeBerief();
-  }, []);
-  const style = {
-    height: "100px",
-  };
+    GetCoutaggeBerief()
+
+}, [])
+const style = {
+
+    height: '100px'
+}
+const handelBack = () => {
+    navigate(-1)
+}
   return (
     <div className=" statbox widget-content widget-content-area">
       <div className="row  ">
         <div className="mb-3 text-center col-12">
           <h4 className="text-center">گزارش بر مبنای کوتاژ</h4>
+          <button className="border-0 bg-transparent  edit-btn float-left" onClick={handelBack}><AiOutlineArrowRight size={'2rem'} /></button>
+
         </div>
         <div className="col-12 ">
           <div className="row mt-4">
@@ -81,59 +88,5 @@ const CoutaggeBrief: React.FC = () => {
 };
 export default CoutaggeBrief;
 
-    useEffect(() => {
-        GetCoutaggeBerief()
-
-    }, [])
-    const style = {
-
-        height: '100px'
-    }
-    const handelBack = () => {
-        navigate(-1)
-    }
-    return (
-        <div className=" statbox widget-content widget-content-area">
-            <div className="row  ">
-                <div className="mb-3 text-center col-12">
-                    <h4 className="text-center text-primary">گزارش بر مبنای کوتاژ</h4>
-                    <button className="border-0 bg-transparent  edit-btn float-left" onClick={handelBack}><AiOutlineArrowRight size={'2rem'} /></button>
-
-                </div>
-                <div>
-                </div>
-                <div className="col-12 ">
-                    <div className="row mt-4">
-
-
-                        <div className="row  text-center">
-
-
-
-
-
-                            {Berief.map((item: any, index: number) =>
-                                <>
-                                    <div className="col-4 text-center">
-                                        <h6 className="mt-4 mb-4" >{`${item.company.companyName}`}</h6>
-                                        <GaugeChart id={` gauge${index}`} percent={(item.supplyAmount.quantity)} textColor='#000' formatTextValue={() => item.supplyAmount.quantity} />
-                                        <p>مقدار عرضه</p>
-                                        <GaugeChart id={` gauge${index}`} percent={(item.salesAmount.quantity)} textColor='#000' formatTextValue={() => item.salesAmount.quantity} />
-                                        <p>مقدار فروش</p>
-
-
-                                        <hr className="my-4" style={{ borderTop: '1px dashed ' }} />
-                                    </div>
-                                </>
-
-                            )}
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-export default CoutaggeBrief
+   
+   
